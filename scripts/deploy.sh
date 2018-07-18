30707298431b16a594c8ebd509d274b93bac3f99
Efi MK <efi.merdler@gmail.com>
2018-07-18 13:14:05 +0300

    Create super user on first deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e
 
 ./manage.py collectstatic --noinput
 # Cheating, making sure that migrate is working
 export LAMBDA_TASK_ROOT=/home
 ./manage.py migrate
+# Create an admin when deploying for the first time
+echo "from django.contrib.auth.models import User; import os; User.objects.create_superuser('root', 'my@email.com', os.environ.get('DJANGO_ADMIN_PASSWORD','MyPassword')) if len(User.objects.filter(email='my@email.com')) == 0 else print('Admin exists')"|./manage.py shell
 # We need the so, Lambda env does not contaon it.
 cp ./scripts/_sqlite3.so .
 sls deploy
